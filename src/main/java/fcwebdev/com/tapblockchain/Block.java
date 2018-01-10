8b0346e99512479a8003d60deebd96ae4d73a713
Filippo Calabrese <filippo.calabrese@stud.unifi.it>
2018-01-10 08:58:39 +0100

    Block.java Implemented and tested

---
+++ src/main/java/fcwebdev/com/tapblockchain/Block.java
@@ -0,0 +1,87 @@
+package fcwebdev.com.tapblockchain;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
+public class Block {
+	private String data, timestamp, previousHash, hash;
+	private String[] algorithm;
+	private boolean hasValidHash;
+	private int index;
+	private int noun = 0;
+
+	public Block(String timestamp, String data, String[] algorithm) {
+		this.timestamp = timestamp;
+		this.data = data;
+		this.algorithm = algorithm;
+		this.previousHash = "";
+		generateHash();
+	}
+	
+	public boolean isHashValid() {
+		return hasValidHash;
+	}
+
+	public int getIndex() {
+		return this.index;
+	}
+
+	public void setIndex(int index) {
+		this.index = index;
+	}
+
+	public void increaseNoun() {
+		this.noun = noun + 1;
+	}
+
+	public String getPreviousHash() {
+		return previousHash;
+	}
+
+	public String getData() {
+		return this.data;
+	}
+
+	public void setPreviousHash(String previousHash) {
+		this.previousHash = previousHash;
+	}
+
+	public String getHash() {
+		return hash;
+	}
+
+	public void generateHash() {
+		try {
+			this.hash = generate(this.index + this.timestamp + this.data + this.noun, algorithm[0]);
+		} catch (NoSuchAlgorithmException e) {
+			declareBlockInvalidHash("error: Invalid algorithm");
+		} catch (UnsupportedEncodingException e) {
+			declareBlockInvalidHash("error: unsupported encoding");
+		}
+	}
+
+	private void declareBlockInvalidHash(String message) {
+		this.hash = message;
+		this.hasValidHash = false;
+	}
+
+	private String generate(String input, String algorithm) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+		MessageDigest md = MessageDigest.getInstance(algorithm);
+		md.reset();
+		byte[] buffer = input.getBytes("UTF-8");
+		md.update(buffer);
+		byte[] digest = md.digest();
+
+		String hexStr = "";
+		for (int i = 0; i < digest.length; i++) {
+			hexStr += Integer.toString((digest[i] & 0xff) + 0x100, 16).substring(1);
+		}
+		return hexStr;
+	}
+
+	public int getNoun() {
+		return this.noun;
+	}
+
+}
---
+++ src/test/java/fcwebdev/com/tapblockchain/BlockTest.java
@@ -0,0 +1,81 @@
+package fcwebdev.com.tapblockchain;
+
+import static org.junit.Assert.*;
+
+import java.security.NoSuchAlgorithmException;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class BlockTest {
+	
+	private Block testBlock;
+	
+	@Before
+	public void init() {
+		String[] algorithm = {"SHA-256", "UTF-8"};
+		testBlock = new Block("1/1/2018", "some data", algorithm);
+	}
+	
+	@Test
+	public void testIndexGetter() {
+		assertEquals(0, testBlock.getIndex());
+	}
+
+	@Test
+	public void testIndexSetter() {
+		testBlock.setIndex(1);
+		assertEquals(1, testBlock.getIndex());
+	}
+	
+	@Test
+	public void testDefaultNounValue() {
+		assertEquals(0, testBlock.getNoun());
+	}
+	
+	@Test
+	public void testNounIncrease() {
+		int previousNoun = testBlock.getNoun();
+		testBlock.increaseNoun();
+		assertEquals(previousNoun+1, testBlock.getNoun());
+	}
+	
+	@Test
+	public void testDataGetter() {
+		assertEquals("some data", testBlock.getData());
+	}
+	
+	@Test
+	public void testHashValidation() {
+		assertNotEquals("", testBlock.getHash());
+	}
+	
+	@Test
+	public void testNoSuchAlgorithmExceptionHandling() {
+		String[] algorithm = {"", "UTF-8"};
+		validateBlockHashWithCustomAlgorithm(false, algorithm);
+	}
+	
+	@Test
+	public void testUnsupportedEncodingExceptionHandling() {
+		String[] algorithm = {"SHA-256", ""};
+		validateBlockHashWithCustomAlgorithm(false, algorithm);
+	}
+	
+	@Test
+	public void testDefaultPreviousHashGetter() {
+		assertEquals("", testBlock.getPreviousHash());
+	}
+	
+	@Test
+	public void testPreviousHashSetter() {
+		testBlock.setPreviousHash("1234");
+		assertEquals("1234", testBlock.getPreviousHash());
+	}
+	
+	private void validateBlockHashWithCustomAlgorithm(boolean supposed, String[] algorithm) {
+		testBlock = new Block("1/1/2018", "some data", algorithm);
+		assertEquals(supposed, testBlock.isHashValid());
+	}
+
+}
